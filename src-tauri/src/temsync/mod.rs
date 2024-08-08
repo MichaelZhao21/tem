813388b2328b8f7b6e73a35db79860e2c2532e56
Michael Zhao <michaelzhao314@gmail.com>
2024-08-07 21:41:25 -0500

    Add main file to ignore warnings

--- src-tauri/src/temsync/mod.rs
+++ src-tauri/src/temsync/mod.rs
@@ -1,4 +1,5 @@
 use meyers::get_diff;
+use patch::{generate_and_write_patch, generate_patch, read_patch};
 
 mod token;
 mod tokenizer;
@@ -6,9 +7,13 @@ mod edit;
 mod meyers;
 mod patch;
 
+// TEMP: To get rid of warnings -- this will not be used until we have server stuff setup TT
 pub fn main() {
     let a = tokenizer::parse_file_tokens("").expect("parsing should work!");
     let b = tokenizer::parse_file_tokens("").expect("parsing should work 2!");
     let diff = get_diff(&a, &b).expect("diff should work");
+    generate_patch(&diff);
+    let _ = generate_and_write_patch(&diff, "");
+    let _ = read_patch("");
     println!("{:?}", diff);
 }
